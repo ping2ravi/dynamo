611d0edb062cae7c96c8155e00c714932a86fa02
Ravi Sharma <ping2ravi@gmail.com>
2016-04-23 18:35:09 +0100

    added gitignore and updated build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
-docker run --name=mysql -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=dynamo --net=host -d mysql:5.5
-mvn clean install
-docker stop mysql
-docker rm mysql
+docker stop mysql-for-build-test
+docker rm mysql-for-build-test
+docker run --name=mysql-for-build-test -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=dynamo --net=host -d mysql:5.5
 
+cwd=$(pwd)
+
+docker stop DynamoBuild
+docker rm DynamoBuild
+
+echo docker run --name=DynamoBuild -v ${cwd}/target:/target -e GIT_REPO=https://github.com/ping2ravi/dynamo.git --net=host ping2ravi/maven-build-docker:latest
+docker run --name=DynamoBuild -v ${cwd}/target:/target -e GIT_REPO=https://github.com/ping2ravi/dynamo.git --net=host ping2ravi/maven-build-docker:latest
+ 
+docker stop mysql-for-build-test
+docker rm mysql-for-build-test
+docker rm DynamoBuild
