c84f55fa78932ecfaddcd147232b267bafd112ee
Ravi Sharma <ping2ravi@gmail.com>
2016-05-06 08:58:35 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ echo mkdir -p ${cwd}/target
 mkdir -p ${cwd}/target
 
 echo docker run --name=DynamoBuild -v ${cwd}/target:/target -e GIT_REPO=https://github.com/ping2ravi/dynamo.git --net=host ping2ravi/maven-build-docker:latest
-docker run --name=DynamoBuild -e MVN_COMMAND="clean verify -P all-tests " -v ${cwd}/target:/target -v ~/.m2:/root/.m2/ -e GIT_REPO=https://github.com/ping2ravi/dynamo.git --net=host ping2ravi/maven-build-docker:latest
+docker run --name=DynamoBuild -e MVN_COMMAND="clean verify -P all-tests " -v ${cwd}/target:/root/target -v ~/.m2:/root/.m2/ -e GIT_REPO=https://github.com/ping2ravi/dynamo.git --net=host ping2ravi/maven-build-docker:latest
  
 docker stop mysql-for-build-test
 docker rm mysql-for-build-test
