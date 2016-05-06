1d1150a8ba34a6b8d899e58c1b82071e5a276fa2
Ravi Sharma <ping2ravi@gmail.com>
2016-05-06 05:02:14 +0100

    Refactored

---
+++ domain/src/main/java/com/next/dynamo/App.java
@@ -0,0 +1,38 @@
+package com.next.dynamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.CurrentDateTimeProvider;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+
+	@Bean
+	public DateTimeProvider dateTimeProvider() {
+		return CurrentDateTimeProvider.INSTANCE;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean localValidatorFactoryBean(MessageSource messageSource) {
+		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+		localValidatorFactoryBean.setValidationMessageSource(messageSource);
+		return localValidatorFactoryBean;
+	}
+
+	
+}
---
+++ domain/src/main/java/com/next/dynamo/context/PersistanceServiceContext.java
@@ -0,0 +1,12 @@
+package com.next.dynamo.context;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages={"com.next.dynamo"})
+public class PersistanceServiceContext {
+
+	
+
+}
