7119e3b7ab49afa68163f5901ba3ee469bab71ff
Ravi Sharma <ping2ravi@gmail.com>
2016-05-07 06:30:55 +0100

    Maven command externalized, to be passed from jenkins

--- build.sh
+++ build.sh
@@ -15,15 +15,15 @@ then
 HOST_JENKINS_HOME=${cwd}
 fi
 
-profile=$1
+MVN_COMMAND=$1
 if [ -z "${profile+xxx}" ];
 then
-profile=all-tests
+MVN_COMMAND=clean verify -P all-tests
 fi
 
-echo docker run --name=DynamoBuild -e MVN_COMMAND="clean verify -P $profile" -e BUILD_OUTPUT=$BUILD_OUTPUT_DIRECTORY -v $HOST_JENKINS_HOME/workspace/${JOB_NAME}/build-output:$BUILD_OUTPUT_DIRECTORY -v ~/.m2:/root/.m2/ -e GIT_REPO=https://github.com/ping2ravi/dynamo.git --net=host ping2ravi/maven-build-docker:latest
+echo docker run --name=DynamoBuild -e MVN_COMMAND="$MVN_COMMAND" -e BUILD_OUTPUT=$BUILD_OUTPUT_DIRECTORY -v $HOST_JENKINS_HOME/workspace/${JOB_NAME}/build-output:$BUILD_OUTPUT_DIRECTORY -v ~/.m2:/root/.m2/ -e GIT_REPO=https://github.com/ping2ravi/dynamo.git --net=host ping2ravi/maven-build-docker:latest
 
-docker run --name=DynamoBuild -e MVN_COMMAND="clean verify deploy -P $profile" -e BUILD_OUTPUT=$BUILD_OUTPUT_DIRECTORY -v $HOST_JENKINS_HOME/workspace/${JOB_NAME}/build-output:$BUILD_OUTPUT_DIRECTORY -v ~/.m2:/root/.m2/ -e GIT_REPO=https://github.com/ping2ravi/dynamo.git --net=host ping2ravi/maven-build-docker:latest
+docker run --name=DynamoBuild -e MVN_COMMAND="$MVN_COMMAND" -e BUILD_OUTPUT=$BUILD_OUTPUT_DIRECTORY -v $HOST_JENKINS_HOME/workspace/${JOB_NAME}/build-output:$BUILD_OUTPUT_DIRECTORY -v ~/.m2:/root/.m2/ -e GIT_REPO=https://github.com/ping2ravi/dynamo.git --net=host ping2ravi/maven-build-docker:latest
  
 docker stop mysql-for-build-test
 docker rm mysql-for-build-test
